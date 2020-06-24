3978c6e00406a6482d48c49429da11410a7881f8
TutorialesSolorzano <solorzano.tutoriales@gmail.com>
2020-06-24 18:49:39 -0500

    agregando docs

---
+++ docs/precache-manifest.5ef65ca7b21593fcd3cc977d3f34024a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f520cdd3149216084ce59323466bcea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61164b683216ce868ade",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61164b683216ce868ade",
+    "url": "/static/js/main.d6bdb3fa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
